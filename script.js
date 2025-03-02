03ccb250c4464c509f37573c2a0082b210a83c9a
JuliAlchemDev <julialchem@gmail.com>
2025-03-02 13:06:27 +0100

    Ex:1 done

--- script.js
+++ script.js
@@ -1,5 +1,42 @@
-const world = 'world';
-export function hello(who = world) {
-    return `Hello ${who}! `;
+"use strict";
+// To compile ' npx tsc '
+// To activate watch mode ' npx tsc -w '
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+function getJoke() {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            const response = yield fetch('https://icanhazdadjoke.com/', {
+                headers: {
+                    "Accept": 'application/json',
+                }
+            });
+            const data = yield response.json();
+            console.log('Joke: ', data.joke);
+            const joke = document.getElementById('joke');
+            if (joke) {
+                joke.textContent = data.joke;
+            }
+            else {
+                console.error('Element with id "joke" not found');
+            }
+        }
+        catch (error) {
+            console.error('Oops! Something went wrong...', error);
+        }
+    });
 }
-console.log(hello());
+const nextJokeBtn = document.getElementById('nextJoke');
+if (nextJokeBtn) {
+    nextJokeBtn.addEventListener('click', getJoke);
+}
+else {
+    console.error('Can\'t find the button... sorry :( ');
+}
--- script.ts
+++ script.ts
@@ -1,6 +1,38 @@
-const world = 'world';
+// To compile ' npx tsc '
+// To activate watch mode ' npx tsc -w '
 
-export function hello(who: string = world): string {
-  return `Hello ${who}! `;
-}
-console.log(hello());
+async function getJoke(): Promise<void> {
+    try {
+      const response = await fetch('https://icanhazdadjoke.com/', {
+        headers: {
+          "Accept": 'application/json',
+        }
+      })
+      const data = await response.json()
+      console.log('Joke: ', data.joke)
+      
+    const joke = document.getElementById('joke');
+     if (joke) {
+      joke.textContent = data.joke;
+     } else {
+      console.error('Element with id "joke" not found');
+     }
+    
+  
+    } catch (error) {
+      console.error('Oops! Something went wrong...', error)
+    }
+  }
+  
+  const nextJokeBtn = document.getElementById('nextJoke');
+    if(nextJokeBtn) {
+      nextJokeBtn.addEventListener('click', getJoke);
+      
+    } else {
+      console.error('Can\'t find the button... sorry :( ');
+    }
+  
+  
+  
+  
+  
